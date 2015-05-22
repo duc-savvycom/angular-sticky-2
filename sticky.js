a37ae35048011c7195705826428dc7fa2c0563fb
Jake Buob <jake@mojotech.com>
2015-05-22 18:24:23 -0400

    Tab with spaces

--- sticky.js
+++ sticky.js
@@ -1,34 +1,34 @@
 angular.module('sticky', [])
 
 .directive('sticky', ['$timeout', function($timeout){
-	return {
-		restrict: 'A',
-		scope: {
-			offset: '@',
-		},
-		link: function($scope, $elem, $attrs){
-			$timeout(function(){
-				var offsetTop = $scope.offset || 0,
-				        $window = angular.element(window);
+  return {
+    restrict: 'A',
+    scope: {
+      offset: '@',
+    },
+    link: function($scope, $elem, $attrs){
+      $timeout(function(){
+        var offsetTop = $scope.offset || 0,
+                $window = angular.element(window);
 
 
-				// Check if our anchor has passed the top of the window
-				//
-				function checkSticky(){
+        // Check if our anchor has passed the top of the window
+        //
+        function checkSticky(){
           if ($elem[0].offsetHeight === 0) return; // return if not visible
 
-					if ($elem.parent()[0].getBoundingClientRect().top - offsetTop <= 0 ) {
-						$elem.css({ top: offsetTop + 'px', position: 'fixed' });
-					} else {
-						$elem.css({ top: '', position: '' });
-					}
-				}
+          if ($elem.parent()[0].getBoundingClientRect().top - offsetTop <= 0 ) {
+            $elem.css({ top: offsetTop + 'px', position: 'fixed' });
+          } else {
+            $elem.css({ top: '', position: '' });
+          }
+        }
 
-				// keep position after changing our sticky element to position fixed
-				$elem.wrap("<div class='sticky-anchor'></div>");
+        // keep position after changing our sticky element to position fixed
+        $elem.wrap("<div class='sticky-anchor'></div>");
 
-				$window.on('scroll', checkSticky);
-			});
-		},
-	};
+        $window.on('scroll', checkSticky);
+      });
+    },
+  };
 }]);
