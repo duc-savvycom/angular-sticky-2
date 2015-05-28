5144aa53868ad59ac0d30d81652a873c3de2382c
Jake Buob <jake@mojotech.com>
2015-05-28 13:38:01 -0400

    Switch to using service for public API

--- sticky.js
+++ sticky.js
@@ -1,57 +1,64 @@
 angular.module('sticky', [])
+.service('Sticky', ['$timeout', function($timeout) {
+  var stickyElements = [];
 
-.directive('sticky', ['$rootScope', '$timeout', function($rootScope, $timeout) {
+  function setAnchorAttrs($elem) {
+    $elem.data('stickyAnchorWidth', window.getComputedStyle($elem.parent()[0], null).getPropertyValue('width'));
+    $elem.data('stickyAnchorHeight', window.getComputedStyle($elem.parent()[0], null).getPropertyValue('height'));
+  }
+
+  function check($elem) {
+    if ($elem[0].offsetHeight === 0) return; // return if not visible
+    var offsetTop = $elem.data('stickyOffset');
+
+    if ($elem.parent()[0].getBoundingClientRect().top - offsetTop <= 0 ) {
+      $elem.css({ top: offsetTop + 'px', position: 'fixed', width: $elem.data('stickyAnchorWidth') });
+      $elem.parent().css({ height: $elem.data('stickyAnchorHeight') }); // prevent visual reflow
+      if (!$elem.hasClass('stuck')) $elem.addClass('stuck');
+    } else {
+      $elem.css({ top: '', position: '', width: '' });
+      $elem.parent().css({ height: '' });
+      if ($elem.hasClass('stuck')) $elem.removeClass('stuck');
+    }
+  }
+
+  return {
+    add: function($elem, offset) {
+      $elem.wrap("<div class='sticky-anchor'></div>");
+      $elem.data('stickyOffset', offset);
+      stickyElements.push($elem);
+      setAnchorAttrs($elem);
+    },
+    reflow: function() {
+      $timeout(function() {
+        stickyElements.forEach(function($elem) {
+          setAnchorAttrs($elem);
+          check($elem);
+        });
+      });
+    },
+    check: check
+  }
+}])
+.directive('sticky', ['Sticky', '$timeout', function(Sticky, $timeout) {
   return {
     restrict: 'A',
     scope: {
       offset: '@',
     },
-    link: function($scope, $elem, $attrs){
-      var anchorWidth;
-      var anchorHeight;
-      var $window = angular.element(window);
-      var offsetTop;
-
-      function setAnchorWidth() {
-        return anchorWidth = window.getComputedStyle($elem.parent()[0], null).getPropertyValue('width');
-      }
-
-      function setAnchorHeight() {
-        return anchorHeight = window.getComputedStyle($elem.parent()[0], null).getPropertyValue('height');
-      }
-
-      // Check if our anchor has passed the top of the window
-      function checkSticky() {
-        if ($elem[0].offsetHeight === 0) return; // return if not visible
-
-        if ($elem.parent()[0].getBoundingClientRect().top - offsetTop <= 0 ) {
-          $elem.css({ top: offsetTop + 'px', position: 'fixed', width: anchorWidth || setAnchorWidth() });
-          $elem.parent().css({ height: anchorHeight || setAnchorHeight() }); // prevent visual reflow
-          if (!$elem.hasClass('stuck')) $elem.addClass('stuck');
-        } else {
-          $elem.css({ top: '', position: '', width: '' });
-          $elem.parent().css({ height: '' });
-          if ($elem.hasClass('stuck')) $elem.removeClass('stuck');
-        }
-      }
-
+    link: function($scope, $elem, $attrs) {
       $timeout(function() {
-        offsetTop = $scope.offset || 0;
+        var offsetTop = $scope.offset || 0;
+        var $window = angular.element(window);
 
-        // keep position after changing our sticky element to position fixed
-        $elem.wrap("<div class='sticky-anchor'></div>");
-        setAnchorWidth();
-        setAnchorHeight();
-        checkSticky();
+        Sticky.add($elem, offsetTop);
 
-        $window.on('scroll', checkSticky);
+        $window.on('scroll', function() {
+          window.requestAnimationFrame(function() {
+            Sticky.check($elem);
+          });
+        });
       });
-
-      $rootScope.$on('angularSticky.reflow', function() {
-        setAnchorWidth();
-        setAnchorHeight();
-        checkSticky();
-      });
-    },
+    }
   };
 }]);
